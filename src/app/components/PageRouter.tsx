f1d43c434d9ea046b4d0e62dc12ccb0794631944
Anton Yatchenko <antony3595@gmail.com>
2022-11-28 17:50:12 +0600

    + remove cra default App component,logo and styles + install sass + add index.scss + Home Page

---
+++ src/app/components/PageRouter.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HomeView from "./views/Home/HomeView";
+
+const PageRouter = () => {
+	return (
+		<div className={"page"}>
+			<HomeView></HomeView>
+		</div>
+	);
+};
+
+export default PageRouter;
---
+++ src/app/components/views/Home/HomeView.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./home.scss";
+
+const HomeView = () => {
+	return (
+		<div className={"home-page"}>
+			<span>Home view</span>
+		</div>
+	);
+};
+
+export default HomeView;
